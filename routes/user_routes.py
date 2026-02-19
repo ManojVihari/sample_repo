edf3d77a2ec6d66148710fab4bee38e628cbf362
Manoj Vihari Machina <manojviharimachina@Manojs-MacBook-Air.local>
2026-02-19 19:38:31 +0530

    renamed api

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -4,7 +4,7 @@ router = APIRouter()
 
 
 @router.post("/create_users")
-def create_user(name: str, email: str, role: str, is_active: bool = True):
+def create_user(name: str, email: str, role: str, is_active: bool):
     """
     Creates a new user with name, email, role, and active status.
 
